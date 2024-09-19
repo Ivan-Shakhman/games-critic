e409db8cf7ea6bd9d387eef5a501bdcf013b708f
Ivan Shakhman <shakhmanivan@gmail.com>
2024-09-19 11:53:11 +0300

    add review model to models in gamesAndReviews

--- gamesAndReviews/models.py
+++ gamesAndReviews/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.db.models import ManyToManyField
 
@@ -23,3 +24,21 @@ class Author(AbstractUser):
 
     def __str__(self):
         return f"{self.pseudonym} ({self.first_name} {self.last_name})"
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)],
+    )
+    date_of_post = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    game_to_review = models.ForeignKey(
+        Game,
+        on_delete=models.CASCADE,
+        related_name="reviews"
+    )
+
+    def __str__(self):
+        return f"({self.author.pseudonym}) {self.title}"
