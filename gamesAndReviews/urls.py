bb8ee65b55ddc0933db35aca23244d5a8088da4f
Ivan Shakhman <shakhmanivan@gmail.com>
2024-09-19 13:51:09 +0300

    add static root and create started skeleton to base.html with empty sidebar

--- gamesAndReviews/urls.py
+++ gamesAndReviews/urls.py
@@ -1,9 +1,11 @@
+from django.conf.urls.static import static
 from django.urls import path
 
+from GamesCritic import settings
 from gamesAndReviews.views import index
 
 urlpatterns = [
-    path("", index)
-]
+    path("", index),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 app_name="games_and_reviews"
