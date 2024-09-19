ecafde28be81dc6e99190007daea6a043e378059
Ivan Shakhman <shakhmanivan@gmail.com>
2024-09-19 12:03:41 +0300

    add game model to models in gamesAndReviews

--- gamesAndReviews/models.py
+++ gamesAndReviews/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
-from django.db.models import ManyToManyField
+from django.db.models import ManyToManyField, DO_NOTHING, SET_NULL
 
 
 class Genre(models.Model):
@@ -26,6 +26,23 @@ class Author(AbstractUser):
         return f"{self.pseudonym} ({self.first_name} {self.last_name})"
 
 
+class Game(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=SET_NULL,
+        related_name="games",
+        null=True
+    )
+    authors = models.ManyToManyField(Author, related_name="games")
+    release_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.name} genre: {self.genre}"
+
+
 class Review(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
@@ -42,3 +59,7 @@ class Review(models.Model):
 
     def __str__(self):
         return f"({self.author.pseudonym}) {self.title}"
+
+
+
+
