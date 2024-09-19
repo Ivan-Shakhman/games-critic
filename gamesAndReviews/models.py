669ff85f5a45e0e6275d07cd08351b716a5cbe1f
Ivan Shakhman <shakhmanivan@gmail.com>
2024-09-19 11:40:17 +0300

    add author model to models in gamesAndReviews

--- gamesAndReviews/models.py
+++ gamesAndReviews/models.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import ManyToManyField
 
 
 class Genre(models.Model):
@@ -9,3 +11,15 @@ class Genre(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Author(AbstractUser):
+    pseudonym = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "author"
+        verbose_name_plural = "authors"
+        ordering = ["pseudonym"]
+
+    def __str__(self):
+        return f"{self.pseudonym} ({self.first_name} {self.last_name})"
