d630bd43c063ad01d766afbdd4ea2088285a9fd8
Ivan Shakhman <shakhmanivan@gmail.com>
2024-09-19 12:56:24 +0300

    register my model in admin and fix timezone

--- gamesAndReviews/admin.py
+++ gamesAndReviews/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
+from gamesAndReviews.models import Game, Genre, Author
+
+admin.site.register(Genre)
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_filter = ("genre", "release_date")
+
+@admin.register(Author)
+class AuthorAdmin(UserAdmin):
+    list_display =UserAdmin.list_display + ("pseudonym",)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Information", {"fields": ("pseudonym",)} ),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "pseudonym",
+                    )
+                },
+            ),
+        )
+    )
 
