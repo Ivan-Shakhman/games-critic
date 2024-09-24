259ddc72c64e89bcf9a0c3d5f9001343166c3f28
Ivan Shakhman <shakhmanivan@gmail.com>
2024-09-24 10:55:13 +0300

    add secret key to .env file

--- GamesCritic/settings.py
+++ GamesCritic/settings.py
@@ -33,9 +33,7 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-9#*_)u95iexs^0in)gc0yn1ku=2-ldaxh38qapa^_k!9!r75$)"
-)
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
